39af30db9dd8c7c27ec563cd83b5347a1a789758
kyahia <yahia.ah.kerdache@gmail.com>
2022-08-14 23:55:22 +0100

    v:2.0

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 import 'fabric';
 
+let textContent;
+
 const canvas = new fabric.Canvas('c');
+document.getElementById('download').addEventListener('click', save);
+document.getElementById('apply').addEventListener('click', apply);
+
+const imgBlob = new Image;
+imgBlob.src = '../assets/image.png';
+imgBlob.crossorigin = "anonymous";
+
+imgBlob.onload = () => {
+   const imgInstance = new fabric.Image(imgBlob);
+   canvas.add(imgInstance);
+}
+
+function apply(){
+if (textContent !== undefined) canvas.remove(textContent);
+   const content = document.getElementById('content').value;
+   const fontSize = Number(document.getElementById('font-size').value);
+   const color = document.getElementById('color').value;
+   let top = Number(document.getElementById('top').value);
+   let left = Number(document.getElementById('left').value);
+   if (top == 0 ) top = 400 - fontSize;
+   if (left == 0 ) left = (400 - fontSize*5.5/2)/2
+
+   textContent = new fabric.Text(content, { top, left, fontSize, fill: color });
+   canvas.add(textContent);
+   canvas.renderAll();
+}
 
 function save() {
    console.log('saving');
@@ -10,21 +38,3 @@ function save() {
    myEl.href = canvas.toDataURL();
    myEl.click();
 }
-
-
-const rect = new fabric.Rect({ left: 100, top: 100, fill: 'red', width: 20, height: 20 });
-canvas.add(rect);
-
-
-const imgBlob = new Image;
-imgBlob.src = '../assets/image.png';
-imgBlob.crossorigin="anonymous";
-
-imgBlob.onload = () => {
-   const imgInstance = new fabric.Image(imgBlob, { left: 1, top: 1 });
-   canvas.add(imgInstance);
-   canvas.renderAll();
-}
-
-document.getElementById('download').addEventListener('click', save);
-
