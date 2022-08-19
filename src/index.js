6655e350b3dc3c1e78509fbd3b080fc5150b97d9
kyahia <yahia.ah.kerdache@gmail.com>
2022-08-19 21:30:24 +0100

    v:3.0 all inputs

--- src/index.js
+++ src/index.js
@@ -1,40 +1,99 @@
 import 'fabric';
 
+const contentEl = document.getElementById('content')
+const fontSizeEl = document.getElementById('font-size')
+const colorEl = document.getElementById('color')
+const topEl = document.getElementById('top')
+const leftEl = document.getElementById('left')
+const separatorEl = document.getElementById('separator');
+
+contentEl.addEventListener('change', apply)
+fontSizeEl.addEventListener('change', apply)
+colorEl.addEventListener('change', apply)
+topEl.addEventListener('change', apply)
+leftEl.addEventListener('change', apply)
+
 let textContent;
+let checked;
+let contentIndex = 0;
+let filename;
 
 const canvas = new fabric.Canvas('c');
 document.getElementById('download').addEventListener('click', save);
 document.getElementById('apply').addEventListener('click', apply);
 
-const imgBlob = new Image;
-imgBlob.src = '../assets/image.png';
-imgBlob.crossorigin = "anonymous";
+const fileInput = document.querySelector('.image-input>input');
+fileInput.addEventListener('change', e => {
+   contentIndex = 0;
+   const reader = new FileReader();
+   reader.onload = () => {
+      filename = e.target.files[0].name;
+      const uploadedImg = reader.result;
 
-imgBlob.onload = () => {
-   const imgInstance = new fabric.Image(imgBlob);
-   canvas.add(imgInstance);
+      const imgBlob = new Image;
+      imgBlob.src = uploadedImg;
+      imgBlob.crossorigin = "anonymous";
+
+      imgBlob.onload = () => {
+         const imgInstance = new fabric.Image(imgBlob);
+         canvas.add(imgInstance);
+         canvas.renderAll();
+      }
+   }
+   reader.readAsDataURL(e.target.files[0]);
+});
+
+
+const fillsList = document.querySelector('form');
+fillsList.addEventListener('submit', e => {
+   e.preventDefault();
+   contentIndex = 0;
+   submitForm()
+});
+
+function submitForm() {
+   fillsList['values-list'].forEach(element => {
+      if (element.checked) checked = element
+   });
+   let textareaFill;
+   if (separatorEl.value == '') textareaFill = checked.parentElement.querySelector('textarea').value.split('\n');
+   else textareaFill = checked.parentElement.querySelector('textarea').value.split(separatorEl.value);
+   console.log(textareaFill)
+   contentEl.value = textareaFill[contentIndex].trim();
+   apply();
 }
 
-function apply(){
-if (textContent !== undefined) canvas.remove(textContent);
-   const content = document.getElementById('content').value;
-   const fontSize = Number(document.getElementById('font-size').value);
-   const color = document.getElementById('color').value;
-   let top = Number(document.getElementById('top').value);
-   let left = Number(document.getElementById('left').value);
-   if (top == 0 ) top = 400 - fontSize;
-   if (left == 0 ) left = (400 - fontSize*5.5/2)/2
+function apply() {
+   if (textContent !== undefined) canvas.remove(textContent);
+   const content = contentEl.value;
+   const fontSize = Number(fontSizeEl.value);
+   const color = colorEl.value;
+   let top = Number(topEl.value);
+   let left = Number(leftEl.value);
+   if (top == 0) top = 400 - fontSize;
+   if (left == 0) left = (400 - fontSize * 5.5 / 2) / 2
 
    textContent = new fabric.Text(content, { top, left, fontSize, fill: color });
    canvas.add(textContent);
    canvas.renderAll();
 }
 
-function save() {
-   console.log('saving');
+function save(i = '') {
    const myEl = document.createElement('a');
    document.body.appendChild(myEl);
-   myEl.download = 'data';
+   myEl.download = filename + i;
    myEl.href = canvas.toDataURL();
    myEl.click();
 }
+
+document.getElementById('download-all').addEventListener('click', () => {
+   const contentList = checked.parentElement.querySelector('label').textContent.split('$');
+   for (let i = 0; i < contentList.length; i++) {
+      setTimeout(() => {
+         submitForm();
+         save(contentList[i].trim());
+         contentIndex += 1;
+      }, i * 100)
+   }
+});
+
